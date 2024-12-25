e16a0b2080272fbd773afb7cb81637ae75ae1aee
Jared Brill <jaredbrill@Jareds-MacBook-Pro.local>
2024-12-24 19:48:08 -0500

    mother chot

---
+++ render-build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+apt-get update && apt-get install -y \
+    libnss3 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libcups2 \
+    libxcomposite1 \
+    libxrandr2 \
+    libxdamage1 \
+    libfontconfig1 \
+    libxkbcommon0 \
+    libasound2 \
+    libpangocairo-1.0-0 \
+    libgtk-3-0 \
+    libgbm1 \
+    libgstreamer-gl1.0-0 \
+    libgstreamer-plugins-bad1.0-0 \
+    libavif15 \
+    libenchant-2-2 \
+    libsecret-1-0 \
+    libmanette-0.2-0 \
+    libgles2-mesa
